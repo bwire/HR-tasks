f128e171da71c926e8f11f948fce182f37776c11
BarbedWire <barbedwire@tut.by>
2017-01-25 18:29:08 +0300

    Scala version commented

--- Functional/Introduction/Scala/FilterPosition.scala
+++ Functional/Introduction/Scala/FilterPosition.scala
@@ -3,6 +3,7 @@
 -- The input and output portions will be handled automatically. You need to write a function with the recommended method signature.
 
 def f(arr: List[Int]): List[Int] = {
+  // the same could be achieved using zipWithIndex arr function
   (List.range(1, arr.length + 1) zip arr)
     .filter(_._1 % 2 == 0)
     .map(_._2)
